4dbe0fe41aa1d4a7be639eec7c4c50d69e02b077
Malicscki <luiz.malicscki@gmail.com>
2022-10-21 18:17:15 -0300

    21/10

--- src/Devs2Blu.Projetos.SistemaCadastro.Forms/Data/PacienteRepository.cs
+++ src/Devs2Blu.Projetos.SistemaCadastro.Forms/Data/PacienteRepository.cs
@@ -50,7 +50,7 @@ namespace Devs2Blu.Projetos.SistemaCadastro.Forms.Data
                 throw;
             }
         }
-        //git commit 20/10
+        //git commit 21/10
         public Paciente UpdatePessoa(Paciente paciente, Int32 idAlterar, MySqlConnection conn)
         {
             try
